38fd98dcb301780622c1cf9e23f547c7c9d15067
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2024-01-04 09:53:16 +0700

    feat: add mining block function

--- test.js
+++ test.js
@@ -3,3 +3,6 @@ const block = new Block("foo", "bar", "zoo", "baz");
 console.log(block.toString());
 
 console.log(Block.genesis().toString());
+
+const fooBlock = Block.mineBlock(Block.genesis(), "foo");
+console.log(fooBlock.toString());
