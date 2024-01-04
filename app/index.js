8b648e8615d3086d0719828a3cdf46bbe2565cef
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2024-01-04 11:49:37 +0700

    feat: blockchain network

--- app/index.js
+++ app/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const P2pServer = require("./p2p-server");
 const Blockchain = require("../blockchain");
 
+const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
+
 // TODO use env
-const HTTP_PORT = 3001;
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 app.use(bodyParser.json());
-
-const bc = new Blockchain();
 
 app.get("/blocks", (req, res) => {
   return res.json(bc.chain);
@@ -18,7 +19,12 @@ app.get("/blocks", (req, res) => {
 app.post("/mine", (req, res) => {
   const block = bc.addBlock(req.body.data);
   console.log(`New block added: ${block.toString()}`);
+  p2pServer.syncChains();
   return res.redirect("/blocks");
 });
 
-app.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
+app.listen(HTTP_PORT, () =>
+  console.log(`App server listening on port: ${HTTP_PORT}`)
+);
+
+p2pServer.listen();
