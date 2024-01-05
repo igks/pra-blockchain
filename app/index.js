ecbc6dbf7c3ab7fa387a795c111ec59ce18e7336
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2024-01-06 06:27:19 +0700

    feat: collect transactions in a pool

--- app/index.js
+++ app/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const P2pServer = require("./p2p-server");
 const Blockchain = require("../blockchain");
+const Wallet = require("../wallet");
+const TransactionPool = require("../wallet/transaction-pool");
 
 const bc = new Blockchain();
-const p2pServer = new P2pServer(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
 
 // TODO use env
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -21,6 +25,21 @@ app.post("/mine", (req, res) => {
   console.log(`New block added: ${block.toString()}`);
   p2pServer.syncChains();
   return res.redirect("/blocks");
+});
+
+app.get("/transactions", (req, res) => {
+  return res.json(tp.transactions);
+});
+
+app.post("/transactions", (req, res) => {
+  const { recipient, amount } = req.body;
+  const transaction = wallet.createTransaction(recipient, amount, tp);
+  p2pServer.broadcastTransaction(transaction);
+  return res.redirect("/transactions");
+});
+
+app.get("/public-key", (req, res) => {
+  return res.json({ publicKey: wallet.publicKey });
 });
 
 app.listen(HTTP_PORT, () =>
